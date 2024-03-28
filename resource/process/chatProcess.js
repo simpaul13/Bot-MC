55cc91ad4d6cc3eda60f5817c2d3dfb3fff80e87
simpaul <28johnpaulsim28@gmail.com>
2024-03-28 22:37:56 +0800

    remove console.log

--- resource/process/chatProcess.js
+++ resource/process/chatProcess.js
@@ -4,15 +4,12 @@ function chatProcess(bot, username, message) {
     if (username !== bot.username) { // Process messages only if they are not from the bot itself
         switch (message) {
             case 'follow':
-                console.log('Follow command received');
                 followPlayerProcess.toggleFollow(bot, username);
                 break;
             case 'stop follow':
-                console.log('Stop Follow command received');
                 followPlayerProcess.stopFollowing(bot); // Ensure stopFollowing is called correctly
                 break;
             case 'hello':
-                console.log('Hello command received');
                 bot.chat('Hello there!');
                 break;
             default:
